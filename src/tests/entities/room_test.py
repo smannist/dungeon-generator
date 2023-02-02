8d21570dd4603560b9117094cb4d15b28bf53f2e
Sami Männistö <sami.mannisto1@gmail.com>
2023-02-02 15:00:06 +0200

    Tests for room constructor

--- src/tests/entities/room_test.py
+++ src/tests/entities/room_test.py
@@ -4,3 +4,11 @@ from entities.room import Room
 class TestRoom(unittest.TestCase):
     def setUp(self):
         self.room = Room(5,5,20,20)
+
+    def test_room_constructor(self):
+        self.assertEqual(self.room.x,5)
+        self.assertEqual(self.room.y,5)
+        self.assertEqual(self.room.height,20)
+        self.assertEqual(self.room.width,20)
+        self.assertEqual(self.room.xy,5+20)
+        self.assertEqual(self.room.yx,5+20)
