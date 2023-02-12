8868b1eba0032e497fc3dd0b87c5239dfbcffc31
Sami Männistö <sami.mannisto1@gmail.com>
2023-02-12 14:35:03 +0200

    Tests for random walk steps

--- src/tests/services/random_walk_test.py
+++ src/tests/services/random_walk_test.py
@@ -26,3 +26,19 @@ class TestRandomWalk(unittest.TestCase):
         for step in walk_coordinates:
             self.assertTrue(0 <= step[0] < self.random_walk.width)
             self.assertTrue(0 <= step[1] < self.random_walk.height)
+    
+    def test_calling_right_step_increases_x_coordinate_accordingly(self):
+        self.assertEqual(self.random_walk.right_step(), 1)
+        self.assertEqual(self.random_walk.x, 76)
+
+    def test_calling_left_step_decreases_x_coordinate_accordigly(self):
+        self.assertEqual(self.random_walk.left_step(), 1)
+        self.assertEqual(self.random_walk.x, 74)
+
+    def test_calling_up_step_decreases_y_coordinate_accordingly(self):
+        self.assertEqual(self.random_walk.up_step(), 1)
+        self.assertEqual(self.random_walk.y, 49)
+
+    def test_calling_down_step_increases_y_coordiate_accordigly(self):
+        self.assertEqual(self.random_walk.down_step(), 1)
+        self.assertEqual(self.random_walk.y, 51)
