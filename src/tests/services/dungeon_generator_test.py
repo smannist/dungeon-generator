1a94c014c3f61f849831632977b3715d8f33f60e
Sami Männistö <sami.mannisto1@gmail.com>
2023-02-14 08:39:34 +0200

    Dungeon generator constructor test

--- src/tests/services/dungeon_generator_test.py
+++ src/tests/services/dungeon_generator_test.py
@@ -7,6 +7,14 @@ class TestDungeonGenerator(unittest.TestCase):
         self.bsp_tree = BSPTree(200,150)
         self.dungeon_generator = DungeonGenerator(self.bsp_tree)
 
+    def test_dungeon_generator_constructor(self):
+        self.assertIsInstance(self.dungeon_generator.bsp_tree, BSPTree)
+        self.assertIsInstance(self.dungeon_generator.root, BSPNode)
+        self.assertEqual(self.dungeon_generator.width, 200)
+        self.assertEqual(self.dungeon_generator.height, 150)
+        self.assertGreater(len(self.dungeon_generator.leaf_nodes), 0)
+        self.assertEqual(self.dungeon_generator.dungeon, [])
+
     def test_dungeon_is_created_with_correct_dimensions(self):
         self.dungeon_generator.initialize_map()
         self.assertEqual(len(self.dungeon_generator.dungeon), 200)
